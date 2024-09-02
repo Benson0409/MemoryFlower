74a590a17a711c84cefc4c7523a9cb595f609793
Benson0409 <benson5508@gmail.com>
2024-09-02 20:23:03 +0800

    Initial Commit

---
+++ MemoryFlower/MemoryFlowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryFlowerApp.swift
+//  MemoryFlower
+//
+//  Created by Benson on 2024/9/2.
+//
+
+import SwiftUI
+
+@main
+struct MemoryFlowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
